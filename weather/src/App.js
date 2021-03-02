882281001f596eca685f6cca109c29c7ad18d133
Jeremy Ludwig <hjeremyludwig@gmail.com>
2021-03-02 10:52:25 -0500

    api request

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,4 +1,6 @@
-// api.openweathermap.org/data/2.5/onecall?lat=43.2557&lon=79.8711&appid=4b861c89a18155e0e8d3c5587cdcad71
+import React, { useEffect, useState } from "react";
+
+// api.openweathermap.org/data/2.5/onecall?lat=42.9849&lon=81.2453&units=metric&appid=4b861c89a18155e0e8d3c5587cdcad71
 
 // api.openweathermap.org/data/2.5/forecast?id=524901&appid=4b861c89a18155e0e8d3c5587cdcad71
 
@@ -7,7 +9,21 @@
 // api.openweathermap.org/data/2.5/weather?lat=79.8711&lon=43.2557&appid=4b861c89a18155e0e8d3c5587cdcad71
 
 function App() {
-    return <div className="App">Hello</div>;
+    const [weatherData, setWeatherData] = useState();
+
+    useEffect(() => {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/onecall?lat=42.9849&lon=81.2453&units=metric&appid=4b861c89a18155e0e8d3c5587cdcad71"
+        )
+            .then(response => response.json())
+            .then(data => setWeatherData(data));
+    }, []);
+
+    return (
+        <div className="App">
+            <div>Temp: {weatherData.current.temp}</div>
+        </div>
+    );
 }
 
 export default App;
