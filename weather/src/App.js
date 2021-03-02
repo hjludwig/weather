82f738df9ab41de655e962deac263530f63f2720
Jeremy Ludwig <hjeremyludwig@gmail.com>
2021-03-02 09:44:51 -0500

    Initial commit

---
+++ weather/src/App.js
@@ -0,0 +1,13 @@
+// api.openweathermap.org/data/2.5/onecall?lat=43.2557&lon=79.8711&appid=4b861c89a18155e0e8d3c5587cdcad71
+
+// api.openweathermap.org/data/2.5/forecast?id=524901&appid=4b861c89a18155e0e8d3c5587cdcad71
+
+// api.openweathermap.org/data/2.5/weather?q=London,ON,CA&appid=4b861c89a18155e0e8d3c5587cdcad71
+
+// api.openweathermap.org/data/2.5/weather?lat=79.8711&lon=43.2557&appid=4b861c89a18155e0e8d3c5587cdcad71
+
+function App() {
+    return <div className="App">Hello</div>;
+}
+
+export default App;
